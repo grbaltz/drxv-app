51f42d6839c600d3aca3a09382f1b6737d7675a2
Garrett Baltz <grbaltz@gmail.com>
2025-05-22 22:11:18 -0700

    Create barebones

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import Stats from '@components/Stats';
+import './styles.scss';
+
+export default function Index() {
+  return (
+    <div className='main'>
+      <Stats/>
+    </div>
+  );
+}
---
+++ src/components/Stats/index.tsx
@@ -0,0 +1,15 @@
+import './Stats.scss';
+
+const Stats = () => {
+
+  return (
+    <div className="statsContainer">
+      <div className="header">GAME #1</div>
+      <div className="field">Field</div>
+      <div className="buttons">Buttons</div>
+      <div className="radialMenuContainer">Cool Radial Menu</div>
+    </div>
+  );
+}
+
+export default Stats;
